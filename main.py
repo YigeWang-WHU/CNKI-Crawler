38024d77f3c9923cefdbba6d8166c01cf19af926
YigeWang-WHU <ygwang328@outlook.com>
2022-01-12 20:21:16 +0800

    bug fix

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ from CrackVerifyCode import crack
 from GetConfig import config
 from GetPageDetail import page_detail
 from userinput import get_uesr_inpt
-
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 HEADER = config.crawl_headers
 # 获取cookie
 BASIC_URL = 'https://kns.cnki.net/kns/brief/result.aspx'
@@ -48,6 +49,7 @@ class SearchTools(object):
 
     def __init__(self):
         self.session = requests.Session()
+        self.session.verify = False
         self.cur_page_num = 1
         # 保持会话
         self.session.get(BASIC_URL, headers=HEADER)
@@ -138,65 +140,72 @@ class SearchTools(object):
         # 定位到内容表区域
         tr_table = soup.find(name='table', attrs={'class': 'GridTableContent'})
         # 处理验证码
-        try:
-            # 去除第一个tr标签（表头）
-            tr_table.tr.extract()
-        except Exception as e:
-            logging.error('出现验证码')
-            return self.parse_page(
-                page_left,
-                crack.get_image(self.get_result_url, self.session,
-                                page_source))
-        # 遍历每一行
-        for index, tr_info in enumerate(tr_table.find_all(name='tr')):
-            tr_text = ''
-            download_url = ''
-            detail_url = ''
-            # 遍历每一列
-            for idx, td_info in enumerate(tr_info.find_all(name='td')):
-                # 因为一列中的信息非常杂乱，此处进行二次拼接
-                td_text = ''
-                for string in td_info.stripped_strings:
-                    if ' ' in string:
-                        string = string.split(' ')[0]
-                    # print(string)
-                    td_text += string
-                tr_text += td_text + ' '
-                with open(
-                        'data/ReferenceList.txt', 'a',
-                        encoding='utf-8') as file:
-                    file.write(td_text + ' ')
-                # 寻找下载链接
-                dl_url = td_info.find('a', attrs={'class': 'briefDl_D'})
-                # 寻找详情链接
-                dt_url = td_info.find('a', attrs={'class': 'fz14'})
-                # 排除不是所需要的列
-                if dt_url:
-                    detail_url = dt_url.attrs['href']
-                if dl_url:
-                    download_url = dl_url.attrs['href']
-            # 将每一篇文献的信息分组
-            single_refence_list = tr_text.split(' ')
-            # print(single_refence_list)
-            print('正在记录: ' + single_refence_list[1])
-            name = single_refence_list[1] + '_' +single_refence_list[2]
-            file_pattern_compile = re.compile(r'[\\/:\*\?"<>\|]')
-            name = re.sub(file_pattern_compile, '', name)
-            self.download_url = DOWNLOAD_URL + re.sub(r'../', '', download_url)
-            #self.download_refence(download_url, single_refence_list)
-            # 是否开启详情页数据抓取
-            if config.crawl_isdetail == '1':
-                time.sleep(config.crawl_stepWaitTime)
-                page_detail.get_detail_page(self.session, self.get_result_url,
-                                            detail_url, single_refence_list,
-                                            self.download_url)
-            # 在每一行结束后输入一个空行
-            with open('data/ReferenceList.txt', 'a', encoding='utf-8') as file:
-                file.write('\n')
-        # page_left为剩余等待遍历页面
-        if page_left > 1:
-            self.cur_page_num += 1
-            self.get_another_page(page_left)
+        if tr_table:
+
+            try:
+                # 去除第一个tr标签（表头）
+                tr_table.tr.extract()
+            except Exception as e:
+                logging.error('出现验证码')
+                return self.parse_page(
+                    page_left,
+                    crack.get_image(self.get_result_url, self.session,
+                                    page_source))
+            # 遍历每一行
+            for index, tr_info in enumerate(tr_table.find_all(name='tr')):
+                tr_text = ''
+                download_url = ''
+                detail_url = ''
+                # 遍历每一列
+                for idx, td_info in enumerate(tr_info.find_all(name='td')):
+                    # 因为一列中的信息非常杂乱，此处进行二次拼接
+                    td_text = ''
+                    for string in td_info.stripped_strings:
+                        if ' ' in string:
+                            string = string.split(' ')[0]
+                        # print(string)
+                        td_text += string
+                    tr_text += td_text + ' '
+                    with open(
+                            'data/ReferenceList.txt', 'a',
+                            encoding='utf-8') as file:
+                        file.write(td_text + ' ')
+                    # 寻找下载链接
+                    dl_url = td_info.find('a', attrs={'class': 'briefDl_D'})
+                    # 寻找详情链接
+                    dt_url = td_info.find('a', attrs={'class': 'fz14'})
+                    # 排除不是所需要的列
+                    if dt_url:
+                        detail_url = dt_url.attrs['href']
+                    if dl_url:
+                        download_url = dl_url.attrs['href']
+                # 将每一篇文献的信息分组
+                single_refence_list = tr_text.split(' ')
+                # print(single_refence_list)
+                print('正在记录: ' + single_refence_list[1])
+                name = single_refence_list[1] + '_' +single_refence_list[2]
+                file_pattern_compile = re.compile(r'[\\/:\*\?"<>\|]')
+                name = re.sub(file_pattern_compile, '', name)
+                self.download_url = DOWNLOAD_URL + re.sub(r'../', '', download_url)
+                #self.download_refence(download_url, single_refence_list)
+                # 是否开启详情页数据抓取
+                if config.crawl_isdetail == '1':
+                    time.sleep(config.crawl_stepWaitTime)
+                    page_detail.get_detail_page(self.session, self.get_result_url,
+                                                detail_url, single_refence_list,
+                                                self.download_url)
+                # 在每一行结束后输入一个空行
+                with open('data/ReferenceList.txt', 'a', encoding='utf-8') as file:
+                    file.write('\n')
+            # page_left为剩余等待遍历页面
+            if page_left > 1:
+                self.cur_page_num += 1
+                self.get_another_page(page_left)
+        else:
+            if page_left > 1:
+                self.cur_page_num += 1
+                self.get_another_page(page_left)
+
 
     def get_another_page(self, page_left):
         '''
